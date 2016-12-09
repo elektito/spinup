9971ea7d994a15e7cf32d990acfc6d063225e777
Mostafa Razavi <mostafa@sepent.com>
2016-12-09 18:10:38 +0330

    Use qemu:///system. Create disk. Check existing.

--- spinup.py
+++ spinup.py
@@ -3,9 +3,69 @@
 import os
 import sys
 import subprocess
+import uuid
 import yaml
 import libvirt
+import xml.etree.ElementTree as ET
 from tempfile import mkdtemp
+
+BASE_IMAGE_DIR = '/var/lib/spinup/images'
+
+xml_template = '''
+<domain type='kvm'>
+  <name>foo</name>
+  <uuid>a1e08189-8d43-495a-85de-079b14781239</uuid>
+  <title>some title</title>
+  <description>some description</description>
+  <metadata>
+    <spinup:instance xmlns:spinup='http://spinup.io/instance'>
+      <spinup:path>{path}</spinup:path>
+    </spinup:instance>
+  </metadata>
+  <os>
+    <type>hvm</type>
+  </os>
+  <memory unit='MiB'>1024</memory>
+
+  <devices>
+    <os>
+      <type>hvm</type>
+    </os>
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2'/>
+      <source file='{disk_image}'/>
+      <backingStore/>
+      <target dev='vda' bus='virtio'/>
+      <alias name='virtio-disk0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+    </disk>
+    <disk type='file' device='cdrom'>
+      <driver name='qemu' type='raw'/>
+      <source file='{config_drive}'/>
+      <backingStore/>
+      <target dev='hda' bus='ide'/>
+      <readonly/>
+      <alias name='ide0-0-0'/>
+      <address type='drive' controller='0' bus='0' target='0' unit='0'/>
+    </disk>
+    <serial type='pty'>
+      <source path='/dev/pts/9'/>
+      <target port='0'/>
+      <alias name='serial0'/>
+    </serial>
+    <console type='pty' tty='/dev/pts/9'>
+      <source path='/dev/pts/9'/>
+      <target type='serial' port='0'/>
+      <alias name='serial0'/>
+    </console>
+    <interface type='bridge'>
+      <source bridge='virbr0'/>
+      <model type='virtio'/>
+    </interface>
+  </devices>
+
+</domain>
+'''
 
 def run_cmd(cmd):
     if isinstance(cmd, str):
@@ -15,20 +75,18 @@ def run_cmd(cmd):
     out, err = proc.communicate()
     return proc.returncode, out, err
 
-def create_cloudconfig_disk():
+def create_cloud_config_drive(machine):
+    print('Creating config drive...')
     with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as f:
         public_key = f.read()
-
-    instance_id = 'foo'
-    hostname = 'foo'
 
     public_key = public_key.strip()
     public_key = public_key.strip()[public_key.index(' ') + 1:]
     public_key = public_key.strip()[:public_key.index(' ')]
 
     meta_data = {
-        'instance-id': instance_id,
-        'local-hostname': hostname
+        'instance-id': machine['instance_id'],
+        'local-hostname': machine['hostname']
     }
 
     meta_data = yaml.dump(meta_data)
@@ -36,13 +94,14 @@ def create_cloudconfig_disk():
     user_data = {
         'ssh_authorized_keys': [
             'ssh-rsa {public_key} ubuntu@{hostname}'.format(
-                public_key=public_key, hostname=hostname)
+                public_key=public_key,
+                hostname=machine['hostname'])
         ]
     }
 
     user_data = '#cloud-config\n\n' + yaml.dump(user_data)
 
-    config_iso = '.config.iso'
+    config_iso = os.path.join(BASE_IMAGE_DIR, machine['id'] + '-config.iso')
     tmpdir = mkdtemp()
     meta_data_file = os.path.join(tmpdir, 'meta-data')
     user_data_file = os.path.join(tmpdir, 'user-data')
@@ -68,92 +127,63 @@ def create_cloudconfig_disk():
 
     return os.path.abspath(config_iso)
 
-def main():
-    libvirt.virEventRegisterDefaultImpl()
-    conn = libvirt.open('qemu:///session')
-    if conn == None:
-        print('Could not connect.', file=sys.stderr)
-        exit(1)
+def get_image(os_type, os_variant):
+    if os_type == 'linux' and os_variant == 'ubuntu':
+        return os.path.join(BASE_IMAGE_DIR, 'xenial-server-cloudimg-amd64-disk1.img')
 
-    xml = '''
-    <domain type='kvm' id='foovm1'>
-      <name>foo</name>
-      <uuid>a1e08189-8d43-495a-85de-079b14781239</uuid>
-      <title>some title</title>
-      <description>some description</description>
-      <os>
-        <type>hvm</type>
-      </os>
-      <memory unit='MiB'>1024</memory>
+def create_disk_image(base_image, machine):
+    print('Creating disk image...')
+    image_filename = os.path.join(BASE_IMAGE_DIR, machine['id'] + '-disk.img')
+    run_cmd('qemu-img create -f qcow2 -b {base_image} {image_filename}'.format(
+        base_image=base_image,
+        image_filename=image_filename))
+    return image_filename
 
-      <devices>
-        <disk type='file' device='disk'>
-          <driver name='qemu' type='qcow2'/>
-          <source file='{disk_image}'/>
-          <backingStore/>
-          <target dev='vda' bus='virtio'/>
-          <alias name='virtio-disk0'/>
-          <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
-        </disk>
-        <disk type='file' device='cdrom'>
-          <driver name='qemu' type='raw'/>
-          <source file='{config_disk}'/>
-          <backingStore/>
-          <target dev='hda' bus='ide'/>
-          <readonly/>
-          <alias name='ide0-0-0'/>
-          <address type='drive' controller='0' bus='0' target='0' unit='0'/>
-        </disk>
-        <serial type='pty'>
-          <source path='/dev/pts/9'/>
-          <target port='0'/>
-          <alias name='serial0'/>
-        </serial>
-        <console type='pty' tty='/dev/pts/9'>
-          <source path='/dev/pts/9'/>
-          <target type='serial' port='0'/>
-          <alias name='serial0'/>
-        </console>
-        <interface type='bridge'>
-          <source bridge='virbr0'/>
-          <model type='virtio'/>
-        </interface>
-      </devices>
+def create_vm(conn, path, machine):
+    machine['id'] = path.replace('/', '-') + '-' + str(uuid.uuid4())
+    machine['id'] = machine['id'][1:]
+    machine['config_drive'] = create_cloud_config_drive(machine)
+    base_image = get_image(machine['os_type'], machine['os_variant'])
+    machine['disk_image'] = create_disk_image(base_image, machine)
 
-    </domain>
-    '''
+    xml = xml_template.format(
+        path=path,
+        config_drive=machine['config_drive'],
+        disk_image=machine['disk_image'])
 
-    config_disk = create_cloudconfig_disk()
-    disk_image = os.path.abspath('disk.img')
-    xml = xml.format(config_disk=config_disk, disk_image=disk_image)
-
+    print('Defining VM...')
     domain = conn.defineXML(xml)
+
+    print('Launching VM...')
     domain.create()
-    stream = conn.newStream(libvirt.VIR_STREAM_NONBLOCK)
-    domain.openConsole(None, stream, 0)
 
-    def stream_callback(stream, events, user_data):
-        try:
-            received_data = stream.recv(1024)
-        except:
-            return
-        os.write(0, received_data)
-    #def stdin_callback(watch, fd, events, unused):
-    #    pass
-    #stdin_watch = libvirt.virEventAddHandle(0, libvirt.VIR_EVENT_HANDLE_READABLE, stdin_callback, None)
-    stream.eventAddCallback(libvirt.VIR_STREAM_EVENT_READABLE, stream_callback, None)
+def main():
+    uri = 'qemu:///system'
 
-    try:
-        while True:
-            ret = libvirt.virEventRunDefaultImpl()
-            if ret < 0:
-                print('Error {}.'.format(ret))
-                break
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print('Closing connection...')
-        conn.close()
+    cwd = os.path.abspath(os.curdir)
+
+    print('Connecting to libvirt at {}...'.format(uri))
+    conn = libvirt.open(uri)
+
+    machine = {
+        'instance_id': 'foo',
+        'hostname': 'foo',
+        'os_type': 'linux',
+        'os_variant': 'ubuntu'
+    }
+
+    for domain_id in conn.listDomainsID():
+        domain = conn.lookupByID(domain_id)
+        metadata = domain.metadata(libvirt.VIR_DOMAIN_METADATA_ELEMENT,
+                                   'http://spinup.io/instance')
+        if metadata:
+            tree = ET.fromstring(metadata)
+            path = tree.find('./path').text
+            if path == cwd:
+                print('VM already exists.')
+                return
+
+    create_vm(conn, cwd, machine)
 
 if __name__ == '__main__':
     main()
